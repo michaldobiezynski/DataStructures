66aadf6c1038c2b3930cf66a345f4c03304ca77b
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-07-10 20:17:51 +0100

    experimental commit from intellij

--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -7,4 +7,13 @@ public class Human {
     int heightInInches;
     String eyeColor;
 
+    public void speak() {
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I am" + heightInInches + " inches tall");
+        System.out.println("I am " + age + " years old");
+        System.out.println("My eye color is " + eyeColor);
+    }
+
+    public void
+
 }
