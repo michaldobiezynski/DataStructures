e57cebf0c61bacce4b19ce66b81a9afd4ff993b6
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-07-19 20:03:46 +0100

    recursive binary search

--- src/main/java/PopularSearchingAlgorithms/Recursion/App.java
+++ src/main/java/PopularSearchingAlgorithms/Recursion/App.java
@@ -9,7 +9,34 @@ public class App {
         //array to be used in testing recursive linear search
         int a[] = {1,3,5,7,9,1,2,8,99};
 
-        recursiveLinearSearch(a,0,9);
+        //recursiveLinearSearch(a,0,2);
+
+        //test recursive binary search
+        int testSearch[]  = {1,2,3,4,7,9,12,18};
+
+        System.out.println
+                (recursiveBinarySearch(testSearch,0,testSearch.length,18));
+    }
+
+    public static int recursiveBinarySearch(int [] a, int p, int r, int x) {
+
+        if(p>r){
+
+            return -1;
+
+        } else {
+
+            int q = (p+r)/2;
+
+            if(a[q] == x){
+                return q;
+            } else if(a[q] > x) {
+                return recursiveBinarySearch(a, p, q-1,  x);
+            } else {
+                return recursiveBinarySearch(a, q+1, r,  x);
+            }
+        }
+
     }
 
     public static void reduceByOne(int n) {
