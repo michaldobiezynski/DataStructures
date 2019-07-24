5fab0805394c32dfc3c9d6410dcd198d6c213422
dobimi <michal.dobiezynski@woodmac.com>
2019-07-24 15:27:36 +0100

    initial setup for quickSort algorithm

---
+++ src/main/java/PopularSortingAlgorithms/QuickSort/App.java
@@ -0,0 +1,27 @@
+package PopularSortingAlgorithms.QuickSort;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int[] inputArray = {12,81,74,43,1098,0,8,92,17,754,912,0,6,4};
+
+        quickSort(inputArray,0,inputArray.length-1);
+
+        System.out.println(Arrays.toString(inputArray));
+
+
+    }
+
+    public static void quickSort(int[] inputArray, int start, int end) {
+
+        
+    }
+
+    public static int partition(int[] inputArray, int start, int end) {
+        return 0;
+    }
+
+}
