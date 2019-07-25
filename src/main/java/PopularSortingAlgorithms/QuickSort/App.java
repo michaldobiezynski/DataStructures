b8204d52b563ba9b795b520c7a2d7f14097484b0
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-07-25 21:11:47 +0100

    implemented quick sort

--- src/main/java/PopularSortingAlgorithms/QuickSort/App.java
+++ src/main/java/PopularSortingAlgorithms/QuickSort/App.java
@@ -17,11 +17,38 @@ public class App {
 
     public static void quickSort(int[] inputArray, int start, int end) {
 
+        if(start < end) {
+            int pp = partition(inputArray, start, end); //index position of the correctly placed value in the array
+            quickSort(inputArray,start,pp-1); // sorts left side of the range
+            quickSort(inputArray,pp+1, end); //sort the right side of the range
+        }
         
     }
 
     public static int partition(int[] inputArray, int start, int end) {
-        return 0;
+
+        int pivot = inputArray[end];
+
+        //in the first iteration i starts at -1
+        int i = start -1;
+
+        for(int j = start; j <= end-1; j++) {
+            if(inputArray[j] <= pivot) {
+                i++;
+                //we are swaping below
+                int ival = inputArray[i];
+                int jval = inputArray[j];
+                inputArray[i] = jval;
+                inputArray[j] = ival;
+            }
+        }
+
+        //put the pivot value in the correct slot next
+        int ival = inputArray[i+1];
+        inputArray[end] = ival;
+        inputArray[i+1] = pivot; //here, pivot value is placed in the correct slot of the array
+
+        return i+1;
     }
 
 }
