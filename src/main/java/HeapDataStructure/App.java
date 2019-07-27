c10c2cf7a9b878853a39e6651176233112f63075
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-07-27 18:55:11 +0100

    display Heap in a tree format

--- src/main/java/HeapDataStructure/App.java
+++ src/main/java/HeapDataStructure/App.java
@@ -4,6 +4,19 @@ public class App {
 
     public static void main(String[] args) {
 
+        Heap newHeap = new Heap(10);
+        newHeap.insert(10);
+        newHeap.insert(11);
+        newHeap.insert(13);
+        newHeap.insert(15);
+        newHeap.insert(16);
+        newHeap.insert(5);
+        newHeap.insert(18);
+        newHeap.insert(30);
+        newHeap.insert(15);
+        newHeap.insert(90);
+
+        newHeap.displayHeap();
     }
 
 
--- src/main/java/HeapDataStructure/Heap.java
+++ src/main/java/HeapDataStructure/Heap.java
@@ -89,6 +89,53 @@ public class Heap {
 
     }
 
+    public void displayHeap() {
+        System.out.println("Heap Array: ");
+        for(int m = 0; m < currentSize; m++) {
+            if(heapArray[m] != null) {
+                System.out.print( heapArray[m].getKey() + " ");
+            }
+            System.out.println();
+
+            int nBlanks = 32;
+            int itemsPerRow = 1;
+            int column = 0;
+            int j = 0; // current item
+
+            String dots = "...............................";
+            System.out.println(dots+dots);
+            while(currentSize > 0) {
+                if(column == 0) {
+                    for(int k = 0; k < nBlanks; k++) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.print(heapArray[j].getKey());
+                j++;
+                if(j == currentSize) {
+                    break;
+                }
+
+                column++;
+                // end of row
+                if(column == itemsPerRow) {
+                    nBlanks = nBlanks/2; // half the blanks
+                    itemsPerRow = itemsPerRow * 2;     // twice the items
+                    column = 0;
+                    System.out.println();
+                } else {
+                    for(int k=0; k<nBlanks*2; k++) {
+                        System.out.print(" ");
+                    }
+                }
+
+            }
+            System.out.println("\n"+dots+dots);
+        }
+
+
+    }
+
 
 }
 
